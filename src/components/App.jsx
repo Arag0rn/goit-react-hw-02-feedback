41983b7f796385c47106d4132019f8a65c487b6b
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2023-08-23 20:41:00 +0200

    finished btn and stat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+
+import { Component } from "react";
+import { FeedbackOptions } from "./FeedbackOptions";
+import { Statistics } from "./Statistic";
+
+export class App extends Component{
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  onLeaveFeedback = (feedbackType) => {
+    this.setState(prevState => {
+     
+        return {
+          ...prevState,
+          [feedbackType]: prevState[feedbackType] + 1,
+        }
+    }
+
+    )
+  }
+  render() {
+    return (
+    <>
+    <FeedbackOptions onLeaveFeedback={this.onLeaveFeedback} items={this.state}/>
+    <Statistics items={this.state}/>
+    </>
+  );}
+}
---
+++ src/components/Statistic.jsx
@@ -0,0 +1,13 @@
+
+
+export const Statistics = ({items}) => {
+     
+        return <>
+          <ul>
+          <li>{items.good}</li>
+          <li>{items.neutral}</li>
+          <li>{items.bad}</li>
+          </ul>
+          </>
+    
+}
